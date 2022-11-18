a8800d4bfc80c36b9f069151b5a5800a877a5d6e
Charlie Saunders <charlieasaunders@gmail.com>
2022-11-18 14:07:01 -0500

    Remove VS Code (using settings sync). Update lots of fish stuff.

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,10 @@ direnv hook fish | source
 
 
 [ -s "/Users/charlessaunders/.jabba/jabba.fish" ]; and source "/Users/charlessaunders/.jabba/jabba.fish"
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+fish_add_path /usr/local/opt/python@3.7/bin
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/charlessaunders/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
