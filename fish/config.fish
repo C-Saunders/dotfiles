704a554762ddd29e05e2caf5242fc2bc9b5f51f4
Charlie Saunders <charles@datavant.com>
2023-03-24 11:00:00 -0400

    Move abbreviations from fish variables to config.
    Abbreviations are no longer stored in universal variables. See https://fishshell.com/docs/current/relnotes.html#deprecations-and-removed-features

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,31 @@ fish_add_path "$HOME/bin"
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "$HOME/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
+# Abbreviations
+## change directory
+abbr -a -U -- home 'cd ~'
+abbr -a -U -- dev 'cd ~/dev/'
+abbr -a -U -- desk 'cd ~/Desktop'
+abbr -a -U -- .. 'cd ..'
+
+## general
+abbr -a -U -- ls 'exa -a'
+abbr -a -U -- ll 'exa -lah'
+abbr -a -U -- cat bat
+abbr -a -U -- c. 'code .'
+abbr -a -U -- o. 'open .'
+
+## git
+abbr -a -U -- gb 'git branch'
+abbr -a -U -- gd 'git diff'
+abbr -a -U -- coma 'git checkout main'
+abbr -a -U -- gcom 'git commit -m'
+abbr -a -U -- gmm 'git merge main'
+abbr -a -U -- gp 'git pull'
+abbr -a -U -- ga 'git add'
+abbr -a -U -- gupd 'git add -A && git commit -n --amend --no-edit'
+abbr -a -U -- gl 'git log'
+abbr -a -U -- gaa 'git add -A'
+abbr -a -U -- gs 'git status'
+abbr -a -U -- co 'git checkout'
