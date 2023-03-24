2c6d13d45708f54996d28b75ed42ac50302a5b23
Charlie Saunders <charles@datavant.com>
2023-03-24 10:29:49 -0400

    Update and clean up fish config path manipulations.

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,12 @@
-direnv hook fish | source
-
-
-[ -s "/Users/charlessaunders/.jabba/jabba.fish" ]; and source "/Users/charlessaunders/.jabba/jabba.fish"
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
-fish_add_path /usr/local/opt/python@3.7/bin
+
+# this might only be correct for m1 Macs
+fish_add_path /opt/homebrew/bin
+
+# for clite
+fish_add_path "$HOME/bin"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/charlessaunders/.rd/bin"
+set --export --prepend PATH "$HOME/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
