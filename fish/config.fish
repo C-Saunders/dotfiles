91ffcda8d516398d8c65170160921654c900fe5e
Charlie Saunders <charles@datavant.com>
2023-03-29 10:06:34 -0400

    Correct comments. Add homebrew/sbin to path.

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,9 @@
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
 
-# this might only be correct for m1 Macs
+# only correct for m1 Macs
 fish_add_path /opt/homebrew/bin
-
-# for clite
-fish_add_path "$HOME/bin"
+fish_add_path /opt/homebrew/sbin
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "$HOME/.rd/bin"
